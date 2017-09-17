848d65f5d0be2ee424bd5b3e7aa15252b745b9dc
hongkabi <924311234@qq.com>
2017-09-17 23:22:19 +0800

    init

---
+++ hhhh.js
@@ -0,0 +1,20 @@
+/**
+ * Created by lirunhua on 2017/5/16.
+ */
+
+
+function create(length) {
+    var a = []
+    for(var i = 0;i<length;i++){
+        a[i] = 0
+    }
+    return a;
+}
+
+function create1(length) {
+    var arr = new Array(length);
+}
+
+create1(20)
+
+//console.log(create(20))
